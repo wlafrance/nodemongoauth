af3d6f874b842b86aa88f472dd298087077ef9c6
Warren LaFrance <lafrance.warren@jcdev.org>
2019-07-10 15:02:03 -0500

    Added new user

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const express = require('express');
-const app = express('');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const express = require("express");
+const app = express("");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+// Import Routes
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
 // Connect to DB
-mongoose.connect(process.env.DB_CONNECT ,
-{ useNewUrlParser: true },
-()=>console.log('connected to db!'));
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
+  console.log("connected to db!")
+);
 
-
-// Import Routes
-const authRoute = require('./routes/auth');
-
+// Middlewares
+app.use(express.json());
 
 // Router Middlewares
-app.use('/api/user',authRoute);
+app.use("/api/user", authRoute);
 
-app.listen(3000, ()=> console.log('Server is up and running'));
+app.listen(3000, () => console.log("Server is up and running"));
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,22 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.post('/register',(req,res)=>{
-    res.send('Register');
+const User = require("../Model/User");
+
+router.post("/register", async (req, res) => {
+  try {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    });
+
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
-
-
+// For this to work you need a body parser (see index.js)
 
 module.exports = router;
-
